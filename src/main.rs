9f868590c74cb7a133e2353d49861d7526406bc2
ysouyno <ysouyno@163.com>
2020-08-01 14:31:15 +0800

    call strerror_r on linux

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
 use std::env;
+
+#[cfg(target_os = "windows")]
 use std::ptr;
+#[cfg(target_os = "windows")]
 use winapi::shared::minwindef;
+#[cfg(target_os = "windows")]
 use winapi::um::{winbase, winnt};
 
+#[cfg(target_os = "linux")]
+extern crate libc;
+
+#[cfg(target_os = "linux")]
+// from https://stackoverflow.com/questions/40710115/how-does-one-get-the-error-message-as-provided-by-the-system-without-the-os-err
+pub fn error_string(errno: i32) -> String {
+    use std::ffi::CStr;
+    use std::os::raw::c_char;
+    use std::os::raw::c_int;
+    use std::str;
+
+    const TMPBUF_SZ: usize = 128;
+    extern "C" {
+        #[cfg_attr(any(target_os = "linux"), link_name = "__xpg_strerror_r")]
+        fn strerror_r(errnum: c_int, buf: *mut c_char, buflen: libc::size_t) -> c_int;
+    }
+
+    let mut buf = [0 as c_char; TMPBUF_SZ];
+    let p = buf.as_mut_ptr();
+
+    unsafe {
+        if strerror_r(errno as c_int, p, buf.len()) < 0 {
+            panic!("strerror_r failed");
+        }
+
+        let p = p as *const _;
+        str::from_utf8(CStr::from_ptr(p).to_bytes())
+            .unwrap()
+            .to_owned()
+    }
+}
+
+#[cfg(target_os = "windows")]
 pub unsafe fn pwstr_to_string(ptr: winnt::PWSTR) -> String {
     use std::slice::from_raw_parts;
     let len = (0_usize..)
@@ -12,7 +49,8 @@ pub unsafe fn pwstr_to_string(ptr: winnt::PWSTR) -> String {
     String::from_utf16_lossy(array)
 }
 
-fn format_message(err_num: i32) -> String {
+#[cfg(target_os = "windows")]
+fn error_string(err_num: i32) -> String {
     let mut err_msg: winnt::LPWSTR = ptr::null_mut();
     let ret = unsafe {
         winbase::FormatMessageW(
@@ -49,5 +87,5 @@ fn main() {
 
     let err = args[1].parse::<i32>().unwrap();
 
-    println!("Error({}): {}", err, format_message(err));
+    println!("Error({}): {}", err, error_string(err));
 }
