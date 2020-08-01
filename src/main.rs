aecc7388b2a629121521488d1f227b96d3ae2b25
ysouyno <ysouyno@163.com>
2020-08-01 10:36:00 +0800

    remove Cargo.lock from gitignore because it is an executable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use winapi::um::{ winbase, winnt };
 use std::ptr;
 use winapi::shared::minwindef;
+use winapi::um::{winbase, winnt};
 
 pub unsafe fn pwstr_to_string(ptr: winnt::PWSTR) -> String {
     use std::slice::from_raw_parts;
@@ -16,25 +16,22 @@ fn format_message(err_num: i32) -> String {
     let mut err_msg: winnt::LPWSTR = ptr::null_mut();
     let ret = unsafe {
         winbase::FormatMessageW(
-            winbase::FORMAT_MESSAGE_ALLOCATE_BUFFER |
-            winbase::FORMAT_MESSAGE_FROM_SYSTEM |
-            winbase::FORMAT_MESSAGE_IGNORE_INSERTS,
+            winbase::FORMAT_MESSAGE_ALLOCATE_BUFFER
+                | winbase::FORMAT_MESSAGE_FROM_SYSTEM
+                | winbase::FORMAT_MESSAGE_IGNORE_INSERTS,
             ptr::null_mut(),
             err_num as u32,
-            winnt::MAKELANGID(winnt::LANG_NEUTRAL,
-                              winnt::SUBLANG_DEFAULT) as u32,
+            winnt::MAKELANGID(winnt::LANG_NEUTRAL, winnt::SUBLANG_DEFAULT) as u32,
             (&mut err_msg as *mut winnt::LPWSTR) as winnt::LPWSTR,
             0,
-            ptr::null_mut()
+            ptr::null_mut(),
         )
     };
 
     if ret == 0 {
         String::from("Unknown")
     } else {
-        let ret = unsafe {
-            pwstr_to_string(err_msg)
-        };
+        let ret = unsafe { pwstr_to_string(err_msg) };
 
         unsafe {
             winbase::LocalFree(err_msg as minwindef::HLOCAL);
